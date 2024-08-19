141a19c5d8c58d69cefdfefc7326ae59c5ee86d3
George Wortmann <gf.wortmann@gmail.com>
2024-08-19 19:11:46 +0300

    Module_7_2.py added

---
+++ Module_7_2/Module_7_2.py
@@ -0,0 +1,24 @@
+# Домашнее задание по теме "Позиционирование в файле".
+
+def custom_write(file_name, strings):
+    file = open(file_name, 'w', encoding='utf-8')
+    string_positions = dict()
+    string_no = 0
+    # string: object
+    for string in strings:
+        pos_before = file.tell()
+        string_no += 1
+        string_positions[(string_no, file.tell())] = string
+        file.write(string + '\n')
+    return string_positions
+
+
+strings = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+result = custom_write('test.txt', strings)
+for elem in result.items():
+    print(elem)
